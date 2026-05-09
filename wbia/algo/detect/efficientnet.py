e665cfb27f9cee78dc8b69bd6b93105419499d9b
Wild Me <jason@wildme.org>
2026-05-09 10:23:19 -0700

    Add seabass labeler v1

--- wbia/algo/detect/efficientnet.py
+++ wbia/algo/detect/efficientnet.py
@@ -41,7 +41,8 @@ ARCHIVE_URL_DICT = {
     'python_effnet_v1': 'https://wildbookiarepository.azureedge.net/models/labeler_python_effnet.v1.zip',
     'elephant_effnet_v0': 'https://wildbookiarepository.azureedge.net/models/labeler_elephant_effnet.v0.zip',
     'python_effnet_v2': 'https://wildbookiarepository.azureedge.net/models/labeler_python_effnet.v2.zip',
-    'python_effnet_v4': 'https://wildbookiarepository.azureedge.net/models/labeler_python_effnet.v4.zip'
+    'python_effnet_v4': 'https://wildbookiarepository.azureedge.net/models/labeler_python_effnet.v4.zip',
+    'seabass_effnet_v1': 'https://wildbookiarepository.azureedge.net/models/labeler_seabass_effnet.v1.zip'
 }
 
 
